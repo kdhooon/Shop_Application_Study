668b784859613c7d9629908c6a14e8bc215a2868
kdhooon <conpulake@sju.ac.kr>
2021-12-28 00:31:53 +0900

    Member 엔티티 개발

---
+++ japshop/src/main/java/jpabook/japshop/service/MemberService.java
@@ -0,0 +1,49 @@
+package jpabook.japshop.service;
+
+import jpabook.japshop.domain.Member;
+import jpabook.japshop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+// 읽기를 할 경우 readOnly = true 쓰기를 할경우 readOnly = false
+@Transactional(readOnly = true) //데이터를 변경하려면 꼭 필요.
+@RequiredArgsConstructor // 생성자 자동 주입
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional
+    public Long join(Member member){        
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //예외 처리
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    
+    //회원 한명 조회
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
